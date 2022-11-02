f4ed72e3c51511c52eebc826802f1efec4a5dbf8
Matti Tahvonen <matti@vaadin.com>
2022-11-02 14:58:27 +0200

    Added test component also for the actual Grid

--- src/test/java/org/vaadin/klaudeta/Address.java
+++ src/test/java/org/vaadin/klaudeta/Address.java
@@ -1,2 +1,50 @@
-package org.vaadin.klaudeta;public class Address {
+package org.vaadin.klaudeta;
+
+public class Address {
+
+    private String id;
+    private String state;
+    private String address;
+    private String name;
+    private String country;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
--- src/test/java/org/vaadin/klaudeta/DemoView.java
+++ src/test/java/org/vaadin/klaudeta/DemoView.java
@@ -1,12 +1,48 @@
 package org.vaadin.klaudeta;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Route("")
 public class DemoView extends Div {
 
     public DemoView() {
+        add(new Span("Lit Pagination"));
         add(new LitPagination());
+        add(new Span("Paginated Grid"));
+
+        PaginatedGrid<Address, ?> grid = new PaginatedGrid<>();
+
+        add(grid);
+
+        grid.addColumn(Address::getId).setHeader("ID");
+        grid.addColumn(Address::getCountry).setHeader("Country").setSortable(true);
+        grid.addColumn(Address::getState).setHeader("State").setSortable(true);
+        grid.addColumn(Address::getName).setHeader("Name").setSortable(true);
+        grid.addColumn(Address::getAddress).setHeader("Address").setSortable(true);
+
+        List<Address> adresses = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Address address = new Address();
+            address.setAddress("Ruukinkatu");
+            address.setCountry("Finland");
+            address.setState("VS");
+            address.setName("Name " + i);
+            address.setId("" + i);
+            adresses.add(address);
+        }
+
+        grid.setItems(adresses);
+
+        // Sets the max number of items to be rendered on the grid for each page
+        grid.setPageSize(7);
+
+        // Sets how many pages should be visible on the pagination before and/or after the current selected page
+        grid.setPaginatorSize(5);
+
     }
 }
